c7da695804692d74107b1d000e8b5717efe7e5c9
esteveznh-design <estevez.nh@ufl.edu>
2026-04-13 19:06:36 -0400

    Added my functions

--- calculator.py
+++ calculator.py
@@ -35,3 +35,34 @@ def logarithm(a, b):
 
 def exponent(a, b):
     return a ** b
+
+
+
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero (a cannot be 0).")
+    return b / a
+
+def log(a, b):
+    if a <= 0:
+        raise ValueError("Logarithm base must be positive.")
+    if a == 1:
+        raise ValueError("Logarithm base cannot be 1.")
+    if b <= 0:
+        raise ValueError("Logarithm argument must be positive.")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
